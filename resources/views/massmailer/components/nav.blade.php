d4580a58fa0ed79e854b721a7eb6c27437e5950e
Tish <tbtizon.chmsu@gmail.com>
2024-11-27 22:09:27 +0800

    Update nav title

--- resources/views/massmailer/components/nav.blade.php
+++ resources/views/massmailer/components/nav.blade.php
@@ -47,17 +47,21 @@
                          <a class="nav-link" href="{{ route('home') }}">
                              <span class="nav-link-icon d-md-none d-lg-inline-block">
                                  <!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                     viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                     stroke-linecap="round" stroke-linejoin="round">
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                     stroke-linecap="round" stroke-linejoin="round"
+                                     class="icon icon-tabler icons-tabler-outline icon-tabler-chart-bar">
                                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                     <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                                     <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                                     <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                                 </svg>
-                             </span>
+                                     <path
+                                         d="M3 13a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+                                     <path
+                                         d="M15 9a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+                                     <path
+                                         d="M9 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+                                     <path d="M4 20h14" />
+                                 </svg> </span>
                              <span class="nav-link-title">
-                                 Home
+                                 Dashboard
                              </span>
                          </a>
                      </li>
