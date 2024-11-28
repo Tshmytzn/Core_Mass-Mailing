86dcbf095c4f3affd7085665289feb86481f632f
Tish <tbtizon.chmsu@gmail.com>
2024-11-28 15:20:14 +0800

    added analytics chart

--- resources/views/massmailer/index.blade.php
+++ resources/views/massmailer/index.blade.php
@@ -23,55 +23,34 @@
                                 Dashboard
                             </h2>
                         </div>
-                        <!-- Page title actions -->
-                        {{-- <div class="col-auto ms-auto d-print-none">
-                            <div class="btn-list">
-                                <span class="d-none d-sm-inline">
-                                    <a href="#" class="btn">
-                                        New view
-                                    </a>
-                                </span>
-                                <a href="#" class="btn btn-primary d-none d-sm-inline-block"
-                                    data-bs-toggle="modal" data-bs-target="#modal-report">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
-                                    Create new report
-                                </a>
-                                <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
-                                    data-bs-target="#modal-report" aria-label="Create new report">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
-                                </a>
-                            </div>
-                        </div> --}}
+
                     </div>
                 </div>
             </div>
             <!-- Page body -->
             <div class="page-body">
                 <div class="container-xl">
+                    <div class="row row-cards">
 
-                    <div class="col-sm-12 col-lg-12">
-                        <div class="card">
-                            <div class="card-body">
-                              <h2 class="text-center"> Email vs Brochure Campaign Summary </h2>
-                              <div id="chart-completion-tasks-10"></div>
+                        <div class="col-5">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h2 class="text-center"> Leads Overview </h2>
+                                    <div id="chart-tasks-overview"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                        <div class="col-7">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h2 class="text-center"> Employee Email Performance Overview </h2>
+                                    <div id="chart-completion-tasks-10"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
             @include('massmailer.components.footer')
@@ -148,6 +127,77 @@
                     '2020-06-25', '2020-06-26'
                 ],
                 colors: [tabler.getColor("primary"), tabler.getColor("red")],
+                legend: {
+                    show: false,
+                },
+            })).render();
+        });
+        // @formatter:on
+    </script>
+
+    <script>
+        // @formatter:off
+        document.addEventListener("DOMContentLoaded", function() {
+            window.ApexCharts && (new ApexCharts(document.getElementById('chart-tasks-overview'), {
+                chart: {
+                    type: "bar",
+                    fontFamily: 'inherit',
+                    height: 420,
+                    parentHeightOffset: 0,
+                    toolbar: {
+                        show: false,
+                    },
+                    animations: {
+                        enabled: false
+                    },
+                },
+                plotOptions: {
+                    bar: {
+                        columnWidth: '50%',
+                    }
+                },
+                dataLabels: {
+                    enabled: false,
+                },
+                fill: {
+                    opacity: 1,
+                },
+                series: [{
+                    name: "A",
+                    data: [44, 32, 48, 72, 60, 16, 22]
+                }],
+                tooltip: {
+                    theme: 'dark'
+                },
+                grid: {
+                    padding: {
+                        top: -20,
+                        right: 0,
+                        left: -4,
+                        bottom: -4
+                    },
+                    strokeDashArray: 4,
+                },
+                xaxis: {
+                    labels: {
+                        padding: 0,
+                    },
+                    tooltip: {
+                        enabled: false
+                    },
+                    axisBorder: {
+                        show: false,
+                    },
+                    categories: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4', 'Sprint 5', 'Sprint 6',
+                        'Sprint 24'
+                    ],
+                },
+                yaxis: {
+                    labels: {
+                        padding: 4
+                    },
+                },
+                colors: [tabler.getColor("primary")],
                 legend: {
                     show: false,
                 },
