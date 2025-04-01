3a35b852c27043dd8402f7f6324194a348d0f18c
Tish <tbtizon.chmsu@gmail.com>
2025-04-02 04:01:54 +0800

    Update remote staff

--- resources/views/MailWithHtml/Remote_staff.blade.php
+++ resources/views/MailWithHtml/Remote_staff.blade.php
@@ -6,19 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-    <style>
-        .staff {
-            background: url("{{ asset('brochure_img/staff.png') }}") no-repeat center center fixed;
-            background-size: auto 100%;
-            width: 100%;
-            height: 100vh;
-        }
-    </style>
 </head>
 
 <body>
-    <div class="staff"></div>
-
+    <div>
+        <img src="{{ asset('brochure_img/staff.png') }}" alt="Staff Image" style="width: 100%; height: auto;">
+    </div>
     <div>
         {!! $signature !!} <!-- This will render the signature HTML -->
     </div>
